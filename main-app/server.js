2187ace1ec050f48d67972489f7afb65a7b3d7fa
eshumanohare <eshumanohare@gmail.com>
2023-09-24 03:11:14 +0530

    app added

---
+++ main-app/server.js
@@ -0,0 +1,62 @@
+// server.js
+
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3001;
+
+const waitingQueue = [];
+
+// Function to handle matchmaking
+const handleMatchmaking = () => {
+  if (waitingQueue.length >= 2) {
+    const [player1, player2] = waitingQueue.splice(
+      Math.floor(Math.random() * waitingQueue.length),
+      2
+    );
+
+    // Simulated room creation (in practice, implement actual room creation logic)
+    const roomIdentifier = `room_${Math.random().toString(36).substring(7)}`;
+
+    // Notify matched players about the room (using WebSockets)
+    io.to(player1).to(player2).emit("match", { roomIdentifier });
+
+    console.log(
+      `Matched players: ${player1} and ${player2}\nRoom: ${roomIdentifier}`
+    );
+  }
+};
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  // Add the connected player to the waiting queue
+  waitingQueue.push(socket.id);
+
+  // Handle matchmaking whenever a player connects
+  handleMatchmaking();
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+
+    // Remove disconnected player from the waiting queue
+    const index = waitingQueue.indexOf(socket.id);
+    if (index !== -1) {
+      waitingQueue.splice(index, 1);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
