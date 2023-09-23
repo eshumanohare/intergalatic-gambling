3de86077eba8a3d60ea3a34a9415cd9c00d92256
eshumanohare <eshumanohare@gmail.com>
2023-09-24 00:56:17 +0530

    error fixed core contract

--- contracts/galactica-core.sol
+++ contracts/galactica-core.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MyToken is ERC721 {
+contract GalacticaCore is ERC721 {
+    
     address public watcher;
     mapping(uint256 => string) planetURIs;
     string private baseURI;
@@ -27,17 +29,11 @@ contract MyToken is ERC721 {
         watcher = msg.sender;
     }
 
-    function mintPlanets(
-        string[] calldata _planetURIs,
-        Planet[] calldata planets
-    ) internal {
-        require(
-            _planetURIs.length == planets.length,
-            "Error: Mismatch in number of planets and URI"
-        );
-        for (uint256 i = 0; i < planets.length; i++) {
-            _mint(watcher, i + 1);
-            setPlanet(i + 1, _planetURIs[i], planets[i]);
+    function mintPlanets(string[] calldata _planetURIs, Planet[] calldata planets) external {
+        require(_planetURIs.length == planets.length, "Error: Mismatch in number of planets and URI");
+        for(uint256 i = 0; i < planets.length; i++) {
+            _mint(watcher, i+1);
+            setPlanet(i+1, _planetURIs[i], planets[i]);
             totalPlanets += 1;
         }
     }
@@ -47,72 +43,40 @@ contract MyToken is ERC721 {
         baseURI = _baseURI;
     }
 
-    function setPlanet(
-        uint256 planetID,
-        string calldata planetURI,
-        Planet calldata planet
-    ) internal {
+    function setPlanet(uint256 planetID, string calldata planetURI, Planet calldata planet) internal {
         require(_exists(planetID), "Error: Planet already minted");
         planetURIs[planetID] = planetURI;
         IdToPlanet[planetID] = planet;
         emit PlanetURISet(planetID, planetURI);
     }
 
-    function planetWar(
-        address player1,
-        address player2,
-        uint256 planetID1,
-        uint256 planetID2,
-        uint256 attribute
-    ) external view returns (address winner) {
-        require(
-            player1 != address(0) &&
-                player2 != address(0) &&
-                (planetID1 > 0 && planetID1 < totalPlanets) &&
-                (planetID2 > 0 && planetID2 < totalPlanets),
-            "Error: Invalid Player details"
-        );
+    function planetWar(address player1, address player2, uint256 planetID1, uint256 planetID2, uint256 attribute) external view returns (address winner) {
+        require(ownerOf(planetID1) == player1,"Error: Buy the Planet first");
+        require(ownerOf(planetID2) == player2, "Error: Buy the Planet first");
+        require(player1 != address(0) && player2 != address(0) && (planetID1 > 0 && planetID1 <= totalPlanets) && (planetID2 > 0 && planetID2 <= totalPlanets), "Error: Invalid Player details");
         require(attribute < 7, "Error: Attribute index is wrong");
+        
         Planet memory planet1 = IdToPlanet[planetID1];
         Planet memory planet2 = IdToPlanet[planetID2];
 
-        if (planetID1 == planetID2) {
+        if(planetID1 == planetID2) {
             return address(0);
         }
 
-        if (attribute == 0) {
-            return
-                planet1.EldritchMystique > planet2.EldritchMystique
-                    ? player1
-                    : player2;
+        if(attribute == 0) {
+            return planet1.EldritchMystique > planet2.EldritchMystique? player1: player2;
         } else if (attribute == 1) {
-            return
-                planet1.AetherialVeil > planet2.AetherialVeil
-                    ? player1
-                    : player2;
+            return planet1.AetherialVeil > planet2.AetherialVeil? player1: player2;
         } else if (attribute == 2) {
-            return
-                planet1.NebulaeEmbrace > planet2.NebulaeEmbrace
-                    ? player1
-                    : player2;
+            return planet1.NebulaeEmbrace > planet2.NebulaeEmbrace? player1: player2;
         } else if (attribute == 3) {
-            return
-                planet1.ChronoNexus > planet2.ChronoNexus ? player1 : player2;
+            return planet1.ChronoNexus > planet2.ChronoNexus? player1: player2;
         } else if (attribute == 4) {
-            return
-                planet1.PyroclasmicCore > planet2.PyroclasmicCore
-                    ? player1
-                    : player2;
+            return planet1.PyroclasmicCore > planet2.PyroclasmicCore? player1: player2;
         } else if (attribute == 5) {
-            return
-                planet1.ZephyrExpanse > planet2.ZephyrExpanse
-                    ? player1
-                    : player2;
+            return planet1.ZephyrExpanse > planet2.ZephyrExpanse? player1: player2;
         } else if (attribute == 6) {
-            return
-                planet1.QuantumEnigma > planet2.QuantumEnigma
-                    ? player1
-                    : player2;
+            return planet1.QuantumEnigma > planet2.QuantumEnigma? player1: player2;
         }
-    }
+    }    
 }
