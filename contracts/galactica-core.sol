0dcb7ef71cc861adc1e6eb7dc7df6e64790bc24d
eshumanohare <eshumanohare@gmail.com>
2023-09-26 00:35:34 +0530

    core contract updated

--- contracts/galactica-core.sol
+++ contracts/galactica-core.sol
@@ -6,11 +6,12 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract GalacticaCore is ERC721 {
     
-    address public watcher;
-    mapping(uint256 => string) planetURIs;
+    address private watcher;
+    mapping(uint256 => string) private  planetURIs;
     string private baseURI;
-    mapping(uint256 => Planet) IdToPlanet;
-    uint256 public totalPlanets;
+    mapping(uint256 => Planet) private IdToPlanet;
+    uint256 private totalPlanets;
+    bool[] status = new bool[](30);
 
     struct Planet {
         string name;
@@ -30,10 +31,11 @@ contract GalacticaCore is ERC721 {
     }
 
     function mintPlanets(string[] calldata _planetURIs, Planet[] calldata planets) external {
+        require(msg.sender == watcher, "Error: Not allowed to mint planets");
         require(_planetURIs.length == planets.length, "Error: Mismatch in number of planets and URI");
         for(uint256 i = 0; i < planets.length; i++) {
-            _mint(watcher, i+1);
-            setPlanet(i+1, _planetURIs[i], planets[i]);
+            _mint(watcher, i);
+            setPlanet(i, _planetURIs[i], planets[i]);
             totalPlanets += 1;
         }
     }
@@ -51,11 +53,12 @@ contract GalacticaCore is ERC721 {
     }
 
     function planetWar(address player1, address player2, uint256 planetID1, uint256 planetID2, uint256 attribute) external view returns (address winner) {
+        require(msg.sender == watcher, "Error: War can be initiated by the watcher only");
         require(ownerOf(planetID1) == player1,"Error: Buy the Planet first");
         require(ownerOf(planetID2) == player2, "Error: Buy the Planet first");
-        require(player1 != address(0) && player2 != address(0) && (planetID1 > 0 && planetID1 <= totalPlanets) && (planetID2 > 0 && planetID2 <= totalPlanets), "Error: Invalid Player details");
+        require(player1 != address(0) && player2 != address(0) && (planetID1 >= 0 && planetID1 <= totalPlanets) && (planetID2 >= 0 && planetID2 <= totalPlanets), "Error: Invalid Player details");
         require(attribute < 7, "Error: Attribute index is wrong");
-        
+
         Planet memory planet1 = IdToPlanet[planetID1];
         Planet memory planet2 = IdToPlanet[planetID2];
 
@@ -75,7 +78,16 @@ contract GalacticaCore is ERC721 {
             return planet1.PyroclasmicCore > planet2.PyroclasmicCore? player1: player2;
         } else if (attribute == 5) {
             return planet1.ZephyrExpanse > planet2.ZephyrExpanse? player1: player2;
-        } 
+        }
+        
         return planet1.QuantumEnigma > planet2.QuantumEnigma? player1: player2;
-    }    
+    }
+
+    function setStatus(uint256 planetID) external {
+        require(msg.sender == watcher, "Error: Not allowed to set status");
+        status[planetID] = true;
+    }
+    function getStatus() external view returns (bool[] memory) {
+        return status;
+    }
 }
